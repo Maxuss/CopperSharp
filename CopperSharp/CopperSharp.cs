f1c9d55d6cb9ed6d9e6e3b3f589cdb2286a54b6c
maxus <maxus.info.mail@gmail.com>
2022-01-05 10:29:52 +0300

    .gitignore + started actually working on project

---
+++ CopperSharp/CopperSharp.cs
@@ -0,0 +1,12 @@
+namespace CopperSharp;
+
+/// <summary>
+/// Class, containing some info on the CopperSharp
+/// </summary>
+public static class CopperSharp
+{
+    /// <summary>
+    /// Current version of CopperSharp
+    /// </summary>
+    public static Version Version { get; } = new(0, 0, 1);
+}
---
+++ CopperSharpExamples/Program.cs
@@ -0,0 +1,10 @@
+﻿using CopperSharp.Modules;
+using Version = CopperSharp.Utils.Version;
+
+[ModuleInfo("ExampleModule", 
+    ID = "example",
+    Authors = new []{ "Maxuss" })]
+public class ExampleModule
+{
+    
+}
